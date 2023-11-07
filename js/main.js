fc268d7bb6f29c4ab3fcdf214ac26d45670c07a7
SoImyr <merov_roma@mail.ru>
2023-11-07 19:12:07 +0300

    GPixel

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  $('.peek__slider-list').slick({
+    arrows: false,
+    slidesToShow: 5,
+    autoplay:true,
+    autoplayspeed:1500,
+    infinite: true,
+  })
+})
